32713d304aae29e12aceeeaecad33aac74c0bab4
Zexi Li <zexi.li@icloud.com>
2025-02-25 10:43:24 +0800

    set comment color

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -8,10 +8,10 @@ local M = {}
 M.base46 = {
 	theme = "github_dark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true, fg = "#999999" },
+		["@comment"] = { italic = true, fg = "#999999" },
+	},
 }
 
 return M
